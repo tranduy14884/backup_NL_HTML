43e90c688b391cd1f9df761cd62b8040c4cfdc87
Duy Tran <tranduy14884@gmail.com>
2021-04-02 18:26:37 +0700

    push list-hotel-page

---
+++ js/carousel.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        dots:false,
+        autoplayTimeout: 1500,
+        smartSpeed: 1500,
+        animateIn: 'linear',
+        animateOut: 'linear',
+
+        autoplay:true,
+        autoplayTimeout: 5000,
+        
+        autoWidth:false,
+        nav:true,
+        navText : ['<i class="fas fa-chevron-circle-left"></i>','<i class="fas fa-chevron-circle-right"></i>'],  
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:4,
+                loop:false
+            }
+        }
+    })
+});
